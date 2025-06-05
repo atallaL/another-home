424b1c58fb370e4b720230fd5a58bef6af0adc79
Leonardo Atalla <latal094@uottawa.ca>
2025-06-05 14:24:25 -0400

    home page additions

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -2,6 +2,9 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 
 import './Home.css';
+import dogImg from '../../assets/homecat.jpg';
+import catImg from '../../assets/homedog.jpeg';
+import tapeImg from '../../assets/tape.png';
 
 function Home() {
     return (
@@ -11,12 +14,33 @@ function Home() {
                     <div className="heroContent col-md-4">
                         <h1>Another Home</h1>
                         <p>We're dedicated to creating the second chances that animals deserve while providing comfort to the people who love them.</p>
+                        <button>Start Exploring</button>
                     </div>
                 </div>
 
             </div>
             <div className="homeContent">
-
+                <div className="homeSection">
+                    <div className="homeSectionContent">
+                        <h2>Want to adopt?</h2>
+                        <p>Do you want to adopt the perfect pet for you? Look through animals that we're currently sheltering and find your next companion today!</p>
+                        <button>Adopt</button>
+                    </div>
+                    <div className="homeSectionImage">
+                        <img src={dogImg}/>
+                    </div>
+                </div>
+                <div className="homeDivider"></div>
+                <div className="homeSection">
+                    <div className="homeSectionImage">
+                        <img src={catImg}/>
+                    </div>
+                    <div className="homeSectionContent">
+                        <h2>Rehome a pet</h2>
+                        <p>We understand how difficult it can be to care for a pet. If you're looking for a safe and loving place for your animal, we will help you every step of the way.</p>
+                         <button>Rehome</button>
+                    </div>
+                </div>
             </div>
         </div>
     );
